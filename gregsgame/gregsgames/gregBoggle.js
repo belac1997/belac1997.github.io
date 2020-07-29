2059ffabb30de58ccaedbc07a35f93df21a7e71d
belac1997 <51928072+belac1997@users.noreply.github.com>
2020-07-28 21:30:11 -0400

    Add files via upload

---
+++ gregsgame/gregsgames/gregBoggle.js
@@ -0,0 +1,31 @@
+function words(x)
+{
+	switch (x)
+	{
+	case 1:
+		var word = new Array("balte","table","hat","tab","belt","lab","eat","tea","ate","tale","bale","let","bet","teal","late","beat");
+		break;		
+	case 2:
+		var word = new Array("atwre","water","wet","wear","tear","war","ret","rate","eat","ate","tea","awe","raw","rat","wart","art","tar");
+		break;
+	case 3:
+		var word = new Array("dclaen","can","cane","and","clan","lane","lean","lend","land","den","dean","dance","lance","clean","deal","ale","dale","candle","clad");
+		break;
+	case 4:
+		var word = new Array("aepinlar","air","airplane","plane","plan","lane","lean","pane","ear","near","nap","nape","lair","pen","pan","ape","leap","ale","peal","nap","rap","par", "pare", "pale", "are", "rail", "ail", "pail", "nail", "air", "pair", "ran", "pin", "pine", "line", "nip", "rip", "ripe", "lip", "earn", "learn", "ire");
+		break;
+	case 5:
+		var word = new Array("redykboa","keyboard","key","board","bored","bore","bark","dark","dork","oar","boar","ark","dare","bare","are","red","rod","road","bode","rode","ode","bread", "read", "bead", "bred", "break", "drey", "day", "boy", "broke", "rake", "bake", "ear", "dear", "bear", "dye", "dyer", "doer", "oak", "boa", "doe", "okay","dab", "bade", "ade", "drake", "bard", "yard", "year", "beak", "beard", "bad", "bed", "bay");
+		break;
+	case 6:
+		var word = new Array("gttorehe","together","to","get","her","hot","hotter","there","hog","rot","the","teeth","ret","goth","got","here","tote","tot","greet","other","three","otter","tree");
+		break;
+	case 7:
+		var word = new Array("ealdpd","paddle","lad","pad","deal","dead","lap","led","dad","pale","pedal","plead","peal","add","ape");
+		break;
+	case 8:
+		var word = new Array("ridlafo","florida","florid","foliar","flair","flora","frail","radio","dial","foil","fair","fold","idol","laid","lair","liar","load","loaf","lord","oral","raid","rail","road","aid","oar","oil","rod");
+		break;	
+	}
+	return word;
+}
